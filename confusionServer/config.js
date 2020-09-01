e3ce34257e0e652eb013d457a9a48a319875fe68
Ramesh <nita.rameshyadav@gmail.com>
2020-09-01 17:36:59 +0530

    Passport-facebook Oauth2

--- confusionServer/config.js
+++ confusionServer/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb+srv://admin:admin123@cluster0.q7nmi.mongodb.net/conFusion'
+    'mongoUrl' : 'mongodb+srv://admin:admin123@cluster0.q7nmi.mongodb.net/conFusion',
+    'facebook': {
+        clientId: '308233530433241',
+        clientSecret: '85b844b3035232c7c522af44526e36b6'
+    }
+    
 }
